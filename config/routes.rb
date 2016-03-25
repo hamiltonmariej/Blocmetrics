0bc01da4e5a27edbbbc3813378c8d75a26dfb96f
Jeannine Hamilton <jeannine.hamilton@gmail.com>
2016-03-25 10:51:06 -0500

    changes to route file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :registered_applications
 
   namespace :api, defaults: { format: :json } do
-    resources :events, only: [:create]
+    resources :events, only: [:create, :options]
   end
 
   match '/events', to: 'events#preflight', via: [:options]
